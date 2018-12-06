0f2262fd5d8d14e8022190d1d0dc7dcfdc5c3de0
github username <github email address>
2018-12-06 12:40:20 -0500

    setup controllers and routes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < ApplicationController
+  before_action :requires_login, only: [:index, :show]
+
+  def index
+    render json: User.all
+  end
+
+  def show
+    @user = User.find_by(id: params[:user_id])
+    render json: @user
+  end
+
+  def create
+    @user = User.new
+
+    @user.username = params[:username]
+    @user.password = params[:password]
+
+      if (@user.save)
+        render json: {
+          username: @user.username,
+          id: @user.id,
+          token: get_token(payload(@user.username, @user.id))
+        }
+      else
+        render json: {
+          errors: @user.errors.full_messages
+        }, status: :unprocessable_entity
+      end
+  end
+
+
+  def user_poems
+    @user = User.find(params[:user_id])
+    render json: @user.poems
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
+  resources :posts
+
+  get '/users/:user_id/poems', to: 'users#user_poems'
+  
+  # This is login:
+  resources :sessions, only: [:create]
+  # post '/sessions/', to: 'sessions#create'
+
+
+
 end
