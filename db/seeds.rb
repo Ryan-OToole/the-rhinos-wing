ce849a7104c46b879ba9d0eabec220d1603a7260
github username <github email address>
2018-12-07 10:14:36 -0500

    added JWT gem setting up auth halfway there

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Jon = User.create(name: "jon", password_digest: "jon")
+Mike = User.create(name: "mike", password_digest: "mike")
+
+Post1 = Post.create(user_id: "1", title: "cheese", body: "poop")
+Post2 = Post.create(user_id: "1", title: "meow", body: "mix")
