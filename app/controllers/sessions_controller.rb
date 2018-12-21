788f4e074b52d0dbe4458f3504749909d07fad45
github username <github email address>
2018-12-21 11:24:18 -0500

    auth working backend

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,6 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(username: params["username"])
 
-    byebug
-
     if (@user && !!@user.authenticate(params["password"]))
       render json: {
         username: @user.username,
